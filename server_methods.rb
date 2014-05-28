f715058ff8430ab57ae9f8b1a9b5d41b3111e907
sskates1 <simonirv2@gmail.com>
2014-05-28 14:27:09 -0400

    Fixed typos

--- server_methods.rb
+++ server_methods.rb
@@ -13,12 +13,12 @@ def db_connection
 end
 
 def get_movies()
-  querry = "SELECT id, title, year, rating, genres.name as genre, stuidos.name as studio
+  query = "SELECT id, title, year, rating, genres.name as genre, stuidos.name as studio
             FROM movies
-            LEFT JOIN sudios on studios.id = movies.stuido_id
+            LEFT JOIN studios on studios.id = movies.stuido_id
             LEFT JOIN genres on genres.id = movies.genre_id"
   movies = db_connection do |conn|
-    conn.exec(querry)
+    conn.exec(query)
   end
   movies = movies.to_a
   movies = movies.sort_by do |movie|
@@ -28,9 +28,9 @@ def get_movies()
 end
 
 def get_actors()
-  querry = "SELECT id, name FROM actors"
+  query = "SELECT id, name FROM actors"
   actors = db_connection do |conn|
-    conn.exec(querry)
+    conn.exec(query)
   end
   actors = actors.to_a
   actors = actors.sort_by do |actor|
@@ -40,42 +40,42 @@ def get_actors()
 end
 
 def get_actor(actor_id)
-  querry = "SELECT actors.id as actorID, movies.id as movieID, actors.name as name, movies.title, character
+  query = "SELECT actors.id as actorID, movies.id as movieID, actors.name as name, movies.title, character
             FROM actors
             JOIN cast_members on cast_members.actor_id = actors.id
             JOIN movies on movies.id = cast_members.movie_id
             WHERE actors.id = $1 "
   actor = db_connection do |conn|
-    conn.exec_params(querry, [actor_id])
+    conn.exec_params(query, [actor_id])
   end
 
-  querry = "SELECT movies.id as movieID, movies.title, character
+  query = "SELECT movies.id as movieID, movies.title, character
             FROM actors
             JOIN cast_members on cast_members.actor_id = actors.id
             JOIN movies on movies.id = cast_members.movie_id
             WHERE actors.id = $1 "
   movies = db_connection do |conn|
-    conn.exec_params(querry, [actor_id])
+    conn.exec_params(query, [actor_id])
   end
 
   return actor, movies
 end
 
 def get_movie(movie_id)
-  querry = "SELECT id, title, year, rating, genres.name as genre, stuidos.name as studio
+  query = "SELECT id, title, year, rating, genres.name as genre, studios.name as studio
             FROM movies
-            LEFT JOIN sudios on studios.id = movies.stuido_id
+            LEFT JOIN studios on studios.id = movies.stuido_id
             LEFT JOIN genres on genres.id = movies.genre_id"
   movie = db_connection do |conn|
-    conn.exec_params(querry, [movie_id])
+    conn.exec_params(query, [movie_id])
   end
-  querry = "SELECT actors.id as actorID, actors.name as actor_name, character
+  query = "SELECT actors.id as actorID, actors.name as actor_name, character
             FROM movies
             JOIN cast_members on cast_members.movie_id = movie.id
             JOIN actors on actors.id = cast_members.actor_id
             WHERE movies.id = $1 "
   cast = db_connection do |conn|
-    conn.exec_params(querry, [movie_id])
+    conn.exec_params(query, [movie_id])
   end
 
   return movie, cast
