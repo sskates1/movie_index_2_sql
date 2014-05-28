361524320d3db9c9307cb3379eb02ebb4420dd3e
K <celfrost@hotmail.com>
2014-05-28 13:22:27 -0400

    server erbs

--- server.rb
+++ server.rb
@@ -1,3 +1,23 @@
 require_relative 'server_methods'
 require sinatra
 require pg
+
+get '/' do
+	erb :index
+end
+
+get '/movies' do
+	erb :movies
+end
+
+get '/:movie' do
+	erb :movie
+end
+
+giet '/actors' do
+	erb :actors
+end
+
+get '/:actor' do
+	erb :actor
+end
